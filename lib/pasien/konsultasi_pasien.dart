99cb5d6e6969c1c0b49820f5471d5ab16b703c8b
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2023-06-08 23:11:30 +0700

    0.1.7
    Refactor `KonsultasiScreen` widget
    
    - Updated the import statement for `GoogleFonts` package.
    - Added additional sample data to the `_loadItems` function.
    - Properly formatted the code for improved readability.
    - Adjusted the height of the `ListView.builder` widget to occupy 60% of the screen height.
    - Wrapped the `ListView.builder` widget with a `SingleChildScrollView` to enable scrolling within the widget.
    - Applied the `GoogleFonts.montserrat` font style to the `item['Keluhan']` text.
    - Adjusted the color and size of the trailing arrow icon in the `ListTile`.
    - Removed unnecessary curly braces in the `onSwipeLeft` and `onSwipeRight` callbacks.

--- lib/pasien/konsultasi_pasien.dart
+++ lib/pasien/konsultasi_pasien.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:swipe/swipe.dart';
 
@@ -29,6 +30,13 @@ class _KonsultasiScreenState extends State<KonsultasiScreen> {
       {'Keluhan': 'Keluhan 1', 'Jawaban': 'Jawaban 1'},
       {'Keluhan': 'Keluhan 2', 'Jawaban': 'Jawaban 2'},
       {'Keluhan': 'Keluhan 3', 'Jawaban': 'Jawaban 3'},
+      {'Keluhan': 'Keluhan 4', 'Jawaban': 'Jawaban 4'},
+      {'Keluhan': 'Keluhan 5', 'Jawaban': 'Jawaban 5'},
+      {'Keluhan': 'Keluhan 6', 'Jawaban': 'Jawaban 6'},
+      {'Keluhan': 'Keluhan 7', 'Jawaban': 'Jawaban 7'},
+      {'Keluhan': 'Keluhan 8', 'Jawaban': 'Jawaban 8'},
+      {'Keluhan': 'Keluhan 9', 'Jawaban': 'Jawaban 9'},
+      {'Keluhan': 'Keluhan 10', 'Jawaban': 'Jawaban 10'},
     ];
   }
 
@@ -42,19 +50,17 @@ class _KonsultasiScreenState extends State<KonsultasiScreen> {
         horizontalMaxHeightThreshold: 500,
         horizontalMinDisplacement: 0,
         horizontalMinVelocity: 0,
-        onSwipeLeft: () => {
-          if (widget.navBarController.index != widget.buildScreensLength - 1)
-            {
-              widget.navBarController
-                  .jumpToTab(widget.navBarController.index + 1)
-            }
+        onSwipeLeft: () {
+          if (widget.navBarController.index != widget.buildScreensLength - 1) {
+            widget.navBarController
+                .jumpToTab(widget.navBarController.index + 1);
+          }
         },
-        onSwipeRight: () => {
-          if (widget.navBarController.index > 0)
-            {
-              widget.navBarController
-                  .jumpToTab(widget.navBarController.index - 1)
-            }
+        onSwipeRight: () {
+          if (widget.navBarController.index > 0) {
+            widget.navBarController
+                .jumpToTab(widget.navBarController.index - 1);
+          }
         },
         child: Column(
           children: [
@@ -63,133 +69,164 @@ class _KonsultasiScreenState extends State<KonsultasiScreen> {
                 color: const Color(0xFFEDF8FE),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
-                      vertical: 12.5, horizontal: 7.5),
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SafeArea(
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 16, right: 16, top: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                const Text(
-                                  "Konsultasi",
-                                  style: TextStyle(
-                                      fontSize: 32,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Container(
-                                  padding: const EdgeInsets.only(
-                                      left: 8, right: 8, top: 2, bottom: 2),
-                                  height: 30,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(30),
-                                    color: Colors.pink[50],
-                                  ),
-                                  child: const Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.add,
-                                        color: Colors.pink,
-                                        size: 20,
-                                      ),
-                                      SizedBox(
-                                        width: 2,
-                                      ),
-                                      Text(
-                                        "Tambah",
-                                        style: TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        Padding(
+                    vertical: 12.5,
+                    horizontal: 7.5,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SafeArea(
+                        child: Padding(
                           padding: const EdgeInsets.only(
-                              top: 16, left: 16, right: 16),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintText: "Cari...",
-                              hintStyle: TextStyle(color: Colors.grey.shade600),
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.grey.shade600,
-                                size: 20,
+                            left: 16,
+                            right: 16,
+                            top: 10,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Text(
+                                "Konsultasi",
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                              filled: true,
-                              fillColor: Colors.grey.shade100,
-                              contentPadding: const EdgeInsets.all(8),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
-                                borderSide:
-                                    BorderSide(color: Colors.grey.shade100),
+                              Container(
+                                padding: const EdgeInsets.only(
+                                  left: 8,
+                                  right: 8,
+                                  top: 2,
+                                  bottom: 2,
+                                ),
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.pink[50],
+                                ),
+                                child: const Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.pink,
+                                      size: 20,
+                                    ),
+                                    SizedBox(
+                                      width: 2,
+                                    ),
+                                    Text(
+                                      "Tambah",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 16,
+                          left: 16,
+                          right: 16,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Cari...",
+                            hintStyle: TextStyle(color: Colors.grey.shade600),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.grey.shade600,
+                              size: 20,
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade100,
+                            contentPadding: const EdgeInsets.all(8),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide(
+                                color: Colors.grey.shade100,
                               ),
                             ),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 16),
-                          child: FutureBuilder<List<Map<String, dynamic>>>(
-                            future: _loadItems(),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const Center(
-                                  child: CircularProgressIndicator(
-                                    color: Colors.pink,
-                                    strokeWidth: 2,
-                                  ),
-                                );
-                              }
-                              if (snapshot.hasError) {
-                                return Text('Error: ${snapshot.error}');
-                              }
-                              final items = snapshot.data ?? [];
-
-                              return ListView.builder(
-                                physics: const NeverScrollableScrollPhysics(),
-                                padding: EdgeInsets.zero,
-                                shrinkWrap: true,
-                                scrollDirection: Axis.vertical,
-                                itemCount: items.length,
-                                itemBuilder: (context, index) {
-                                  final item = items[index];
-                                  return Padding(
-                                    padding: const EdgeInsets.only(bottom: 3.0),
-                                    child: ListTile(
-                                      trailing:
-                                          const Icon(Icons.arrow_forward_ios),
-                                      onTap: () {},
-                                      contentPadding:
-                                          const EdgeInsets.symmetric(
-                                              horizontal: 16),
-                                      dense: true,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                        side: const BorderSide(
-                                            color: Colors.grey, width: 3),
-                                      ),
-                                      tileColor: Colors.white,
-                                      title: Text(item['Keluhan']),
-                                      subtitle: Text(item['Jawaban']),
-                                    ),
-                                  );
-                                },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: FutureBuilder<List<Map<String, dynamic>>>(
+                          future: _loadItems(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                  color: Colors.pink,
+                                  strokeWidth: 2,
+                                ),
                               );
-                            },
-                          ),
+                            }
+                            if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            }
+                            final items = snapshot.data ?? [];
+
+                            return SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.6,
+                              child: SingleChildScrollView(
+                                child: ListView.builder(
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  padding: EdgeInsets.zero,
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.vertical,
+                                  itemCount: items.length,
+                                  itemBuilder: (context, index) {
+                                    final item = items[index];
+                                    return Padding(
+                                      padding: const EdgeInsets.only(
+                                        bottom: 3.0,
+                                      ),
+                                      child: ListTile(
+                                        trailing: const Icon(
+                                          Icons.arrow_forward_ios,
+                                          color: Colors.grey,
+                                          size: 20,
+                                        ),
+                                        onTap: () {},
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                          horizontal: 16,
+                                        ),
+                                        dense: true,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          side: const BorderSide(
+                                            color: Colors.grey,
+                                            width: 3,
+                                          ),
+                                        ),
+                                        tileColor: Colors.white,
+                                        title: Text(
+                                          item['Keluhan'],
+                                          style: GoogleFonts.montserrat(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 16),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
